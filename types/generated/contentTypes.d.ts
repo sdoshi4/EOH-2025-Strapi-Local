c6c2534e388b2312fa7883c26f3becadd91e1797
Shaan Doshi <shaandoshi4@gmail.com>
2025-03-04 20:18:19 -0600

    made "Tags" in exhibits

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -460,9 +460,7 @@ export interface ApiExhibitExhibit extends Struct.CollectionTypeSchema {
     publishedAt: Schema.Attribute.DateTime;
     Sub_Building_A: Schema.Attribute.String;
     Sub_Building_B: Schema.Attribute.String;
-    Tag1: Schema.Attribute.String;
-    Tag2: Schema.Attribute.String;
-    Tag3: Schema.Attribute.String;
+    Tags: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
